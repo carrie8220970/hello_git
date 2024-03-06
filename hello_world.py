6fb9c93c1045ab2c92bb41f64bc23cac66a67af1
“carrie” <carrie8220970@gmail.comgit config --global user.name carriegit config --global user.email carrie8220970@gmail.com>
2024-03-06 23:11:29 +0100

    First branch creation
    learned new functions: git commit, git log, git add, status

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,5 @@
 print("hello git")
 print("This is going to be my first commit ever!!!!!")
 print("I am so happy to be introduced to this new world!!!")
+
+# First commit on the first barnch (this is a typo and will be corrected in the following commit)
