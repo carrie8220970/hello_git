d36650481ccb39fd3404ad8d92fd4d74cb81a892
“carrie” <carrie8220970@gmail.comgit config --global user.name carriegit config --global user.email carrie8220970@gmail.com>
2024-03-06 23:11:29 +0100

    First branch creation
    learned new functions: git commit, git log, git add, status

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,6 @@ print("hello git")
 print("This is going to be my first commit ever!!!!!")
 print("I am so happy to be introduced to this new world!!!")
 
+# There was a conflict here and we were lazy to pick out witch would stay, so both stay. Yay!!!!!
 # Where did my .sql files go?????
+# First commit on the first barnch (this is a typo and will be corrected in the following commit)
